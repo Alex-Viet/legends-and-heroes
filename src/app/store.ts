352c6cbc9d9d86439e346690654341e506ae4975
Alex-Viet <alex.night.power@gmail.com>
2024-07-09 12:32:36 +0300

    Like/dislike + like filter created

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { universeHPotterApi } from '../features/harry-potter/characters/charactersSlice';
+import likesReducer from '../features/harry-potter/likes/likesSlice';
 
 export const store = configureStore({
   reducer: {
     [universeHPotterApi.reducerPath]: universeHPotterApi.reducer,
+    likes: likesReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       white: '#ffffff',
+      black: '#000000',
     },
   },
   plugins: [],
